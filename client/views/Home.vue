e8eaa217910f090fe7e53d3024febdd2975a997b
brokyo <alexcarusillo@gmail.com>
2018-07-18 15:13:48 -0400

    starting

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="page">
-    <counter></counter>
     <p>
-      To get started, edit files in <code>./client</code> and save.
+      Awaken
     </p>
   </div>
 </template>
 
 <script>
-import Counter from 'components/Counter'
-
 export default {
-  components: {
-    Counter
-  }
 }
 </script>
